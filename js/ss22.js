73c285c6a79b941887afd075eea2c9dc0808b3db
kaijo-joho <218201000+kaijo-joho@users.noreply.github.com>
2025-09-19 14:40:27 +0900

    updated by GAS 2025-09-19 14:40:26

--- js/ss22.js
+++ js/ss22.js
@@ -1,90 +1,81 @@
 const slidesData = [
   {
-    "section": "1. 条件に一致するセルを数える\nCOUNTIF関数"
-  },
-  {
-    "title": "1.1. COUNTIF関数",
-    "note": "",
+    "title": "Slide 3",
+    "note": "<strong>COUNTIF関数</strong>は、セル範囲の中から、条件を満たしているセルの個数を返す関数です。<br><pre class=\"codeInDescription\"><code>=COUNTIF(範囲, 条件)</code></pre><br>",
     "image": "img_slide/ss22_03.png"
   },
   {
-    "title": "〔参考〕 ワイルドカード文字",
-    "note": "",
+    "title": "Slide 4",
+    "note": "<br>",
     "image": "img_slide/ss22_04.png"
   },
   {
-    "title": "例題1. アイスクリーム屋のアンケート結果の集計",
-    "note": "",
+    "title": "Slide 5",
+    "note": "<br>",
     "image": "img_slide/ss22_05.png"
   },
   {
-    "title": "例題1. アイスクリーム屋のアンケート結果の集計",
+    "title": "Slide 6",
     "note": "<br>",
     "image": "img_slide/ss22_06.png",
     "showInDetails": true
   },
   {
-    "section": "2. 条件に一致するの合計値・平均値\nSUMIF関数・AVERAGEIF関数"
-  },
-  {
-    "title": "2.1. SUMIF関数・AVERAGEIF関数",
-    "note": "",
+    "title": "Slide 8",
+    "note": "<br>",
     "image": "img_slide/ss22_08.png"
   },
   {
-    "title": "例題2. お小遣い帳",
-    "note": "",
+    "title": "Slide 9",
+    "note": "<br>",
     "image": "img_slide/ss22_09.png"
   },
   {
-    "title": "例題2. お小遣い帳",
+    "title": "Slide 10",
     "note": "<br><br>",
     "image": "img_slide/ss22_10.png",
     "showInDetails": true
   },
   {
-    "title": "例題3. クラス平均",
-    "note": "",
+    "title": "Slide 11",
+    "note": "<br>",
     "image": "img_slide/ss22_11.png"
   },
   {
-    "title": "例題3. クラス平均",
-    "note": "",
+    "title": "Slide 12",
+    "note": "<br>",
     "image": "img_slide/ss22_12.png"
   },
   {
-    "section": "3. 複雑な条件に一致するセルの計算\n*IFS関数"
-  },
-  {
-    "title": "3.1. *IFS関数",
-    "note": "",
+    "title": "Slide 14",
+    "note": "<br>",
     "image": "img_slide/ss22_14.png"
   },
   {
-    "title": "3.1. COUNTIFS関数",
-    "note": "",
+    "title": "Slide 15",
+    "note": "<br>",
     "image": "img_slide/ss22_15.png"
   },
   {
-    "title": "例題4. アンケート結果の集計",
-    "note": "",
+    "title": "Slide 16",
+    "note": "<br>",
     "image": "img_slide/ss22_16.png"
   },
   {
-    "title": "例題4. アンケート結果の集計",
-    "note": "",
+    "title": "Slide 17",
+    "note": "<br>",
     "image": "img_slide/ss22_17.png"
   },
   {
-    "title": "例題4. アンケート結果の集計",
-    "note": "",
+    "title": "Slide 18",
+    "note": "<br>",
     "image": "img_slide/ss22_18.png"
   },
   {
-    "title": "例題4. アンケート結果の集計",
+    "title": "Slide 19",
     "note": "<br><br>",
     "image": "img_slide/ss22_19.png",
     "showInDetails": true
   }
 ]
-window.slidesData = slidesData;;
+window.slidesData = slidesData;
