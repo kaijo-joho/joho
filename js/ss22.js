da4f20b7f44c1024e43b5ee28b9476c336fb6430
kaijo-joho <218201000+kaijo-joho@users.noreply.github.com>
2025-09-26 13:43:06 +0900

    updated by GAS 2025-09-26 13:43:05

--- js/ss22.js
+++ js/ss22.js
@@ -1,78 +1,87 @@
 const slidesData = [
   {
-    "title": "Slide 3",
+    "section": "1. 条件に一致するセルを数える COUNTIF関数"
+  },
+  {
+    "title": "1.1. COUNTIF関数",
     "note": "<strong>COUNTIF関数</strong>は、セル範囲の中から、条件を満たしているセルの個数を返す関数です。<br><pre class=\"codeInDescription\"><code>=COUNTIF(範囲, 条件)</code></pre><br>",
     "image": "img_slide/ss22_03.png"
   },
   {
-    "title": "Slide 4",
+    "title": "〔参考〕 ワイルドカード文字",
     "note": "<strong>ワイルドカード文字</strong>は、部分的に一致する文字列を検索するための記号です。<br>",
     "image": "img_slide/ss22_04.png"
   },
   {
-    "title": "Slide 5",
+    "title": "例題1. アイスクリーム屋のアンケート結果の集計",
     "note": "<br>",
     "image": "img_slide/ss22_05.png"
   },
   {
-    "title": "Slide 6",
+    "title": "例題1. アイスクリーム屋のアンケート結果の集計",
     "note": "<br>",
     "image": "img_slide/ss22_06.png",
     "showInDetails": true
   },
   {
-    "title": "Slide 8",
+    "section": "2. 条件に一致するの合計値・平均値 SUMIF関数・AVERAGEIF関数"
+  },
+  {
+    "title": "2.1. SUMIF関数・AVERAGEIF関数",
     "note": "<br>",
     "image": "img_slide/ss22_08.png"
   },
   {
-    "title": "Slide 9",
+    "title": "例題2. お小遣い帳",
     "note": "<br>",
     "image": "img_slide/ss22_09.png"
   },
   {
-    "title": "Slide 10",
+    "title": "例題2. お小遣い帳",
     "note": "<br><br>",
     "image": "img_slide/ss22_10.png",
     "showInDetails": true
   },
   {
-    "title": "Slide 11",
+    "title": "例題3. クラス平均",
     "note": "<br>",
     "image": "img_slide/ss22_11.png"
   },
   {
-    "title": "Slide 12",
+    "title": "例題3. クラス平均",
     "note": "<br>",
     "image": "img_slide/ss22_12.png"
   },
   {
-    "title": "Slide 14",
+    "section": "3. 複雑な条件に一致するセルの計算 *IFS関数"
+  },
+  {
+    "title": "3.1. *IFS関数",
     "note": "<br>",
     "image": "img_slide/ss22_14.png"
   },
   {
-    "title": "Slide 15",
+    "title": "3.1. COUNTIFS関数",
     "note": "<br>",
     "image": "img_slide/ss22_15.png"
   },
   {
-    "title": "Slide 16",
+    "title": "例題4. アンケート結果の集計",
     "note": "<br>",
     "image": "img_slide/ss22_16.png"
   },
   {
-    "title": "Slide 17",
+    "title": "例題4. アンケート結果の集計",
     "note": "<br>",
     "image": "img_slide/ss22_17.png"
   },
   {
-    "title": "Slide 18",
+    "title": "例題4. アンケート結果の集計",
     "note": "<br>",
     "image": "img_slide/ss22_18.png"
   },
   {
-    "title": "Slide 19",
+    "title": "例題4. アンケート結果の集計",
     "note": "<br><br>",
     "image": "img_slide/ss22_19.png",
     "showInDetails": true
